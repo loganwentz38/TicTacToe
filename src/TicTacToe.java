e264759ce1e27c663a986e7bd0e62afbba0598b4
loganwentz38 <113048686+loganwentz38@users.noreply.github.com>
2024-09-24 18:17:30 -0500

    displays board

---
+++ src/TicTacToe.java
@@ -0,0 +1,26 @@
+public class TicTacToe {
+    private static final int MAX_ENTRIES = 9;
+    private static int[][] board = {{1, 2, 3} , {4, 5, 6} , {7, 8, 9,}};
+    private char player;
+    private boolean gameOver;
+    private char computer;
+
+
+    public static void main(String[] args) {
+        for (int j = 1; j < 10; j++) {
+            System.out.printf("%d ", j);
+            if (j % 3 == 0) {
+                System.out.println();
+            }
+        
+        for (int lp = 0; lp < board.length; lp ++) 
+        {
+            for(int i = 0; i < board.length; i++){
+                
+            }
+        }
+    }
+    }
+
+
+}
